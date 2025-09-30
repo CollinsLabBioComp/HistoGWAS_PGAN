a81d1d334169af9804bbba9311f4ef17c9d7bc6e
Andrew Denny <dennyal@biowulf.nih.gov>
2025-09-30 17:49:28 -0400

    embedding works

--- pretrain.py
+++ pretrain.py
@@ -49,82 +49,110 @@ def freeze_lstlayer(class_names):
     DINO.fc = layer 
 
     for name, layer in DINO.named_children():
-        if name in ['conv1', 'bn1', 'relu', 'maxpool', 'layer1', 'layer2', 'layer3']:
+        if name in ['conv1', 'bn1', 'relu', 'maxpool', 'layer1', 'layer2']:
             for param in layer.parameters():
                 param.requires_grad = False
 
 
+    #DINO.to(DEVICE)
 
 
-def train_model(model, criterion, optimizer, scheduler, dataloaders, num_epochs=25):
+
+
+def train_model(model, criterion, optimizer, scheduler, dataloaders, dataset_sizes, out_dir, num_epochs=25):
     since = time.time()
+    os.makedirs(out_dir, exist_ok=True)
+    best_model_params_path = os.path.join(out_dir, 'best_model_params.pt')
+    
 
-    with TemporaryDirectory() as tempdir:
-        best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
+    torch.save(model.state_dict(), best_model_params_path)
+    best_acc = 0.0
 
-        torch.save(model.state_dict(), best_model_params_path)
-        best_acc = 0.0
+    print(dataset_sizes)
 
-        dataset_sizes = {"train": len(dataloaders['train']), "val": len(dataloaders['val'])}
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs - 1}')
+        print('-' * 10)
 
-        for epoch in range(num_epochs):
-            print(f'Epoch {epoch}/{num_epochs - 1}')
-            print('-' * 10)
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  
+            else:
+                model.eval()   
 
-            for phase in ['train', 'val']:
-                if phase == 'train':
-                    model.train()  
-                else:
-                    model.eval()   
+            running_loss = 0.0
+            running_corrects = 0
 
-                running_loss = 0.0
-                running_corrects = 0
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(DEVICE)
+                labels = labels.to(DEVICE)
 
-                # Iterate over data.
-                for inputs, labels in dataloaders[phase]:
-                    inputs = inputs.to(DEVICE)
-                    labels = labels.to(DEVICE)
+                optimizer.zero_grad()
 
-                    # zero the parameter gradients
-                    optimizer.zero_grad()
+                # forward
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
 
-                    # forward
-                    # track history if only in train
-                    with torch.set_grad_enabled(phase == 'train'):
-                        outputs = model(inputs)
-                        _, preds = torch.max(outputs, 1)
-                        loss = criterion(outputs, labels)
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
 
-                        # backward + optimize only if in training phase
-                        if phase == 'train':
-                            loss.backward()
-                            optimizer.step()
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                _, labels = torch.max(labels.data, 1)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
 
-                    # statistics
-                    running_loss += loss.item() * inputs.size(0)
-                    running_corrects += torch.sum(preds == labels.data)
-                if phase == 'train':
-                    scheduler.step()
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-                epoch_loss = running_loss / dataset_sizes[phase]
-                epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
 
-                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                torch.save(model.state_dict(), best_model_params_path)
 
-                # deep copy the model
-                if phase == 'val' and epoch_acc > best_acc:
-                    best_acc = epoch_acc
-                    torch.save(model.state_dict(), best_model_params_path)
+        print("done with epoch")
 
-            print()
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val Acc: {best_acc:4f}')
 
-        time_elapsed = time.time() - since
-        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
-        print(f'Best val Acc: {best_acc:4f}')
-
-        # load best model weights
-        model.load_state_dict(torch.load(best_model_params_path, weights_only=True))
+    # load best model weights
+    model.load_state_dict(torch.load(best_model_params_path))
     return model
+
+
+
+def eval():
+    freeze_lstlayer(['WT/WT', "WT/-", "-/-","CT", "TT"])
+    DINO.load_state_dict(torch.load("./embeddings/model_checkpoints/best_model_params.pt"))
+    DINO.eval() 
+    DINO.to(DEVICE)
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5]),])
+    ad_data = AnnDataset(DF_FILE, transform=transform)
+
+
+    og_ad = AnnDataset(DF_FILE, transform)
+    img, label = og_ad.__getitem__(0)
+    with torch.no_grad():
+        img.to(DEVICE)
+        output = DINO(img)
+
+    _, pred = torch.max(output, axis = 1)
+    _, actual = torch.max(label, axis = 1)
+    print(pred)
+    print(actual)
+    return output
+
+
 
 
 def start(epochs):
@@ -142,6 +170,7 @@ def start(epochs):
     print(val_size)
     print(train_size + val_size)
     print("Total images found", len(ad_data))
+    dataset_sizes = {"train": train_size, "val": val_size}
 
     train_data, val_data = random_split(ad_data, lengths=[train_size, val_size])
     train_load = DataLoader(train_data, batch_size = 32, shuffle=True)
@@ -163,10 +192,10 @@ def start(epochs):
     optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, DINO.parameters()), lr =1e-3)
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-    fine_tuned_dino = train_model(DINO, criterion, optimizer, exp_lr_scheduler, dataloaders,
-                       num_epochs=epochs)
+    fine_tuned_dino = train_model(DINO, criterion, optimizer, exp_lr_scheduler, dataloaders, dataset_sizes= dataset_sizes,
+                     out_dir="/data/dennyal/repos/HistoGWAS_PGAN/embeddings/model_checkpoints",   num_epochs=epochs)
 
 
 
 if __name__ == "__main__":
-    start(epochs=10)    
+    eval()
